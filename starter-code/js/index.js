63bfe2954a9222711e4923921388d0c29c64b018
Rita Mernissi <rita.mernissi@hotmail.com>
2018-10-06 10:35:33 +0200

    modif js

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -49,4 +49,17 @@ window.onload = function(){
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+
+  calculatePriceButton.onclick = function (
+
+    // On récupère le prix, la quantité et on calcule le prix total
+    var productPrice = document.getElementById('product-price');
+    var productQuantity = document.getElementById('product-quantity');
+
+    var productTotal = productPrice.textContext * productQuantity.textContent;
+    return productTotal;
+    // On ajoute le total dans le champs prix total
+    //var total = document.getElementById('product-total');
+    //total.innerHTML = productTotal;
+    )   
 };
